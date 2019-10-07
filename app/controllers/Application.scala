fa87b7afe9baafe0bfe06344e2b68a4b301f4b5d
mohare993 <matthew.o'hare@academytrainee.com>
2019-10-07 12:26:20 +0100

    basic routes done

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,15 @@
 package controllers
 
-import play.api._
 import play.api.mvc._
 
 class Application extends Controller {
 
   def index = Action {
     Ok(views.html.index("Your new application is ready."))
+  }
+
+  def contact = Action {
+    Ok(views.html.contact())
   }
 
 }
