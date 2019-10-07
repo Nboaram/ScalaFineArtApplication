8edc6cc3ea157fed0b2e96d9192fd9ec2362dfa2
johnny <johnny-peckover@live.co.uk>
2019-10-07 16:51:08 +0100

    Capatilise class name

--- app/controllers/navigation/Header.scala
+++ app/controllers/navigation/Header.scala
@@ -2,7 +2,7 @@ package controllers.navigation
 
 import play.api.mvc._
 
-class header extends Controller{
+class Header extends Controller{
 
   def header() = Action {
     Ok(views.html.header("Header")(false))
