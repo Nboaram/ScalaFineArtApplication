6b250e462d9354f9072a9dae09924a701e8c33b1
mohare993 <matthew.o'hare@academytrainee.com>
2019-10-07 11:20:10 +0100

    GalleryController added

--- app/controllers/GalleryController.scala
+++ app/controllers/GalleryController.scala
@@ -1,9 +1,19 @@
 package controllers
 
-import play.api.mvc.Controller
+import play.api.mvc.{Action, Controller}
 
 class GalleryController extends Controller {
 
+  def gallery = Action {
+    Ok(views.html.gallery())
+  }
 
+  def view = Action {
+    Ok(views.html.view())
+  }
+
+  def appraisal = Action {
+    Ok(views.html.appraisal())
+  }
 
 }
--- app/controllers/PaymentController.scala
+++ app/controllers/PaymentController.scala
@@ -1,8 +1,12 @@
 package controllers
 
-import play.api.mvc.Controller
+import play.api.mvc.{Action, Controller}
 
 class PaymentController extends Controller {
+
+  def checkout = Action {
+    Ok(views.html.checkout())
+  }
 
 
 }
