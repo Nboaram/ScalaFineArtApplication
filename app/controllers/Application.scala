abf05957ed20358cab188eaa3e5522131ccd411a
tvaidotas <tadas.vaidotas@gmail.com>
2019-10-08 15:23:27 +0100

    should work

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,14 +1,19 @@
 package controllers
 
+import akka.stream.Materializer
 import helpers.Constants
+import javax.inject.Inject
+import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc._
+import services.MongoServices
 
-class Application extends Controller {
+class Application @Inject()
+(val messagesApi: MessagesApi, val materializer: Materializer, val mongoServices: MongoServices) extends Controller
+  with I18nSupport {
 
-  def index: Action[AnyContent] = Action {
-    request =>
-      request.flash.get(Constants.login.toString).map(welcomeMessage => Ok(views.html.index(welcomeMessage)))
-        .getOrElse(Ok(views.html.index(Constants.indexString.toString)))
+  def index: Action[AnyContent] = Action { implicit request =>
+    request.flash.get(Constants.login.toString).map(welcomeMessage => Ok(views.html.index(welcomeMessage)))
+      .getOrElse(Ok(views.html.index(Constants.indexString.toString)))
   }
 
   def contact = Action {
