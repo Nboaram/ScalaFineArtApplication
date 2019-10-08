282630f9b70516db30b796d786078431e968818f
johnny <johnny-peckover@live.co.uk>
2019-10-08 14:44:08 +0100

    generateId test init

--- test/helpers/GenerateIdTest.scala
+++ test/helpers/GenerateIdTest.scala
@@ -1,5 +1,16 @@
 package helpers
 
-class GenerateIdTest {
+import scala.resources.BaseTestingClass
+
+class GenerateIdTest extends BaseTestingClass {
+
+  object TestObject extends GenerateId {
+    var idCount = 0
+    generateId()
+  }
+
+  "The generateId method" should "add one to the count of the object it is called within" in {
+    assert(TestObject.generateId() == 1)
+  }
 
 }
