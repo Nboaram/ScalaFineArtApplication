2bb98207cd925c269c9a822381215123788405e9
rameezjhaveri <rameezjhaveri94@gmail.com>
2019-10-08 13:50:47 +0100

    Solved merged conflicts for dev merge #2

--- app/controllers/UserController.scala
+++ app/controllers/UserController.scala
@@ -12,8 +12,10 @@ class UserController @Inject()
   with I18nSupport {
 
   def account: Action[AnyContent] = AuthenticatedAction { request =>
-    Ok(views.html.account()
-      s"Hello ${request.session.get("username").getOrElse(Constants.emptyString.toString)}")
+    Ok(views.html.account(request.session.get(Constants.username.toString).getOrElse(Constants.emptyString.toString)))
+
+
   }
 
 }
+//s"Hello ${request.session.get("username").getOrElse(Constants.emptyString.toString)}")
