8886aca75e71fbe608e0d243ce9ebce36a87e9f0
JayKL <Jaykonrad-lee@hotmail.com>
2019-10-09 13:56:41 +0100

    save current progress

--- app/controllers/GalleryController.scala
+++ app/controllers/GalleryController.scala
@@ -1,9 +1,13 @@
 package controllers
 
+import akka.stream.Materializer
 import helpers.Constants
+import javax.inject.Inject
+import play.api.i18n.{I18nSupport, Lang, MessagesApi}
 import play.api.mvc.{Action, Controller}
 
-class GalleryController extends Controller {
+class GalleryController @Inject()
+(val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
   def gallery  = Action {implicit request =>
     Ok(views.html.gallery(Constants.imagesPlaceholder.toString))
--- app/controllers/PaymentController.scala
+++ app/controllers/PaymentController.scala
@@ -1,8 +1,11 @@
 package controllers
 
+import javax.inject.Inject
+import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, Controller}
 
-class PaymentController extends Controller {
+class PaymentController @Inject()
+(val messagesApi: MessagesApi) extends Controller with I18nSupport  {
 
   def checkout = Action { implicit request =>
     Ok(views.html.checkout())
