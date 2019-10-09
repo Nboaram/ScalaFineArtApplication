d4e9b9226ca3f51c9aff03cb32073dd6aa85aba6
frobian21 <frobian21@yahoo.co.uk>
2019-10-09 11:17:15 +0100

    enums for Art categories

---
+++ app/helpers/ArtGenre.scala
@@ -0,0 +1,6 @@
+package helpers
+
+object ArtGenre extends Enumeration {
+  type ArtGenre = Value
+  val Abstract, Cityscape, Genre, Illustration, Landscape, Nude, Portrait, Religious, SketchAndStudy, StillLife = Value
+}
---
+++ app/helpers/ArtMovement.scala
@@ -0,0 +1,15 @@
+package helpers
+
+object ArtMovement extends Enumeration {
+  type ArtMovement = Value
+  val Impressionism, Cubism, Contemporary, Fauvism, Expressionism, Dadaism, Surrealism, Realism, Abstract, Memes = Value
+  val PostImpressionism: ArtMovement.Value = Value("Post-Impressionism")
+  val PopArt: ArtMovement.Value = Value("Pop Art")
+
+  def main(args:Array[String]): Unit ={
+    ArtMovement.values.foreach(println(_))
+    val interests:Array[ArtMovement.Value] = Array(ArtMovement.Abstract,ArtMovement.Cubism)
+    interests.foreach(interest => println(s"I like $interest"))
+  }
+}
+
