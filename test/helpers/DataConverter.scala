44c392823c13c86621b1a0878e6d0d7f11b5afee
johnny <johnny-peckover@live.co.uk>
2019-10-09 09:30:04 +0100

    DataConverter class

---
+++ test/helpers/DataConverter.scala
@@ -0,0 +1,18 @@
+package helpers
+
+import java.io.ByteArrayOutputStream
+
+import scalaz.std.effect.outputStream
+
+import scala.reflect.io.File
+
+class DataConverter {
+
+  def imageToBinaryData(file:File): Unit = {
+
+    ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+
+  }
+
+}
