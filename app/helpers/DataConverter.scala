a1b40b2446da8d844e33880ae931212b61040d07
johnny <johnny-peckover@live.co.uk>
2019-10-09 11:19:41 +0100

    binaryDataToImage needs to be tested

--- app/helpers/DataConverter.scala
+++ app/helpers/DataConverter.scala
@@ -1,16 +1,25 @@
 package helpers
 
-import java.io.{ByteArrayOutputStream, File}
+import java.awt.image.BufferedImage
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream, File}
 
 import javax.imageio.ImageIO
 
 class DataConverter {
 
-  def imageToBinaryData(filename:String): Array[Byte] = {
+  def imageToBinaryData(filename: String): Array[Byte] = {
 
     val outputStream: ByteArrayOutputStream = new ByteArrayOutputStream()
 
-    ImageIO.write(ImageIO.read(new File(filename)), filename.slice(filename.length-3, filename.length), outputStream)
+    ImageIO.write(ImageIO.read(new File(filename)), filename.slice(filename.length - 3, filename.length), outputStream)
     outputStream.toByteArray
   }
+
+  def binaryDataToImage(binaryData: Array[Byte], filename: String) = {
+
+    val byteArrayInputStream :ByteArrayInputStream = new ByteArrayInputStream(binaryData)
+    val bufferedImage :BufferedImage = ImageIO.read(byteArrayInputStream)
+    ImageIO.write(bufferedImage, filename.slice(filename.length - 3, filename.length), new File(filename))
+  }
+
 }
