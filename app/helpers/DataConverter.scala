42f742053af7cc84d02675539a62aa4089fb808e
johnny <johnny-peckover@live.co.uk>
2019-10-10 11:31:08 +0100

    separated file location and name

--- app/helpers/DataConverter.scala
+++ app/helpers/DataConverter.scala
@@ -8,11 +8,11 @@ class DataConverter {
 
   //TODO finalise input type for imageToBinaryData
 
-  def imageToBinaryData(filename: String): Array[Byte] = {
+  def imageToBinaryData(fileLocation: String, filename: String): Array[Byte] = {
 
     val outputStream: ByteArrayOutputStream = new ByteArrayOutputStream()
 
-    ImageIO.write(ImageIO.read(new File(filename)), filename.slice(filename.length - 3, filename.length), outputStream)
+    ImageIO.write(ImageIO.read(new File(fileLocation + filename)), filename.slice(filename.length - 3, filename.length), outputStream)
     outputStream.toByteArray
   }
 
