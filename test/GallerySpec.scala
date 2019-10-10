17db1afaa9ab830af5a434aa233a1aad84df8218
phtb233 <stephen_621@msn.com>
2019-10-10 11:02:25 +0100

    resolved test errors

--- test/GallerySpec.scala
+++ test/GallerySpec.scala
@@ -1,3 +1,4 @@
+import controllers.routes
 import helpers.Constants
 import org.specs2.mutable._
 import play.api.test.Helpers._
@@ -9,24 +10,24 @@ class GallerySpec extends Specification {
   "Gallery" should {
 
     "render the gallery page" in new WithApplication {
-      val gallery = route(FakeRequest(GET, Constants.galleryUrl.toString)).get
+      val gallery = route(FakeRequest(GET, routes.GalleryController.gallery().path())).get
 
       status(gallery) must equalTo(OK)
-      contentType(gallery) must beSome.which(_ == Constants.text_html.toString)
+      contentType(gallery) must beSome.which(_ == HTML)
     }
 
     "render the view page" in new WithApplication {
-      val view = route(FakeRequest(GET, Constants.viewUrl.toString)).get
+      val view = route(FakeRequest(GET, routes.GalleryController.view().path())).get
 
       status(view) must equalTo(OK)
-      contentType(view) must beSome.which(_ == Constants.text_html.toString)
+      contentType(view) must beSome.which(_ == HTML)
     }
 
     "render the appraisal page" in new WithApplication {
-      val appraisal = route(FakeRequest(GET, Constants.appraisalUrl.toString).withSession("username" -> "user")).get //TODO make this a better test
+      val appraisal = route(FakeRequest(GET, routes.GalleryController.appraisal().path()).withSession("username" -> "user")).get //TODO make this a better test
 
       status(appraisal) must equalTo(OK)
-      contentType(appraisal) must beSome.which(_ == Constants.text_html.toString)
+      contentType(appraisal) must beSome.which(_ == HTML)
     }
   }
 
--- test/UserSpec.scala
+++ test/UserSpec.scala
@@ -1,3 +1,4 @@
+  import controllers.routes
   import helpers.Constants
   import org.specs2.mutable._
   import play.api.test.Helpers._
@@ -9,17 +10,17 @@
     "User" should {
 
       "render the login page" in new WithApplication {
-        val login = route(FakeRequest(GET, Constants.loginUrl.toString)).get
+        val login = route(FakeRequest(GET, routes.LoginController.login().path)).get
 
         status(login) must equalTo(OK)
-        contentType(login) must beSome.which(_ == Constants.text_html.toString)
+        contentType(login) must beSome.which(_ == HTML)
       }
 
       "render the account page" in new WithApplication {
-        val account = route(FakeRequest(GET, Constants.accountUrl.toString).withSession("username" -> "user")).get  //TODO make this a better test
+        val account = route(FakeRequest(GET, routes.UserController.account().path()).withSession("username" -> "user")).get  //TODO make this a better test
 
         status(account) must equalTo(OK)
-        contentType(account) must beSome.which(_ == Constants.text_html.toString)
+        contentType(account) must beSome.which(_ == HTML)
       }
     }
 
