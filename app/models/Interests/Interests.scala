33b1adb2a33e8f220afc79598d72ec8dd03e5c29
fabian <frobian21@yahoo.co.uk>
2019-10-14 19:01:23 +0100

    Removed commented out code

--- app/models/Interests/Interests.scala
+++ app/models/Interests/Interests.scala
@@ -1,11 +1,7 @@
 package models.Interests
 
-import java.io
-
-import models.SignUp
 import play.api.data.Form
 import play.api.data.Forms._
-
 import scala.collection.mutable
 
 case class Interests(artType:List[String], artGenre:List[String], artMovements:List[String])
@@ -25,49 +21,3 @@ object  Interests{
     for (elem <- interestsDatabase) {println(elem)}
   }
 }
-
-//
-//object InterestLists{
-////  val artTypeForm: Form[List[ArtType.Value]] = Form(
-////    mapping(
-////      ArtType.Drawings.toString -> boolean,
-////      ArtType.Paintings.toString -> boolean,
-////      ArtType.Photographs.toString -> boolean,
-////      ArtType.Prints.toString -> boolean,
-////      ArtType.Sculptures.toString -> boolean
-////    )()
-////  )
-//
-//  def apply[T](listElements:T*):List[T]=listElements.toList
-//  def unapplySeq[T](list: Seq[T]):Option[String]= list match {
-//  case list:Seq[ArtType] => Some(listToString(list))
-//  case list:Seq[ArtGenre] => Some(listToString(list))
-//  case list:Seq[ArtMovement] => Some(listToString(list))
-//  case _ => None
-//  }
-//  def listToString[T](list:Seq[T], string:String=""):String = {
-//    if (list.isEmpty) ""
-//    else list.head.toString + listToString(list.tail)
-//  }
-//  def main(args:Array[String]):Unit = {
-//    var x = InterestLists(ArtType.Paintings)
-//    println(InterestLists.unapplySeq(x))
-//    x = x :+ ArtType.Drawings
-//    println(InterestLists.unapplySeq(x))
-//    x = x :+ ArtType.Paintings
-//    println(InterestLists.unapplySeq(x))
-//    x = x :+ ArtType.Prints
-//    println(InterestLists.unapplySeq(x))
-//    val loginForm = Form(
-//      ArtType.values.map(value=> (value.toString, boolean):_*
-//    )
-//    val allTypes = ArtType.values.map(value=> value.toString -> "true").toMap
-//    println(allTypes)
-//    val anyData = Map(ArtType.Sculptures.toString -> "true")
-//    val user2 = loginForm.bind(anyData).get
-//    println(user2)
-//    val user3 = loginForm.bind(allTypes)
-//    println(user3)
-//
-//  }
-//}
