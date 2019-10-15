4ca76c68126601ffa832d2039e98df81a6b0a50b
Johnny Peckover <johnny-peckover@live.co.uk>
2019-10-15 16:37:58 +0100

    fileUpload method

---
+++ app/controllers/FileUploadController.scala
@@ -0,0 +1,22 @@
+package controllers
+
+import java.nio.file.Paths
+
+import javax.inject.Inject
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, Controller}
+
+class FileUploadController @Inject()(implicit val messagesApi: MessagesApi) extends Controller with I18nSupport {
+
+  def fileUpload = Action(parse.multipartFormData) { implicit request =>
+    request.body.file("file").map { file =>
+      file.ref.moveTo(Paths.get(s"public/temp/gallery"+file.filename).toFile, replace = true)
+      Ok("Upload successful")
+    }.getOrElse {
+      Redirect(routes.FileUploadController.index).flashing(
+        "error" -> "File not found")
+    }
+  }
+
+
+}
